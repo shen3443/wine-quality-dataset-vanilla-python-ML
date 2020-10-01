a7e5e1c4ea10d9a56968185311e0a90cf643cfab
shen3443 <chris@isacs.net>
2020-10-01 15:19:39 -0500

    Add files via upload
    Uploading files part II

---
+++ neuron.py
@@ -0,0 +1,54 @@
+from activation import sigmoid, relu, leakyrelu
+
+class Neuron:
+    '''
+    Neurons take a number of inputs: X = [x1,x2,x3...xn] and produce 1 output: y
+    via feedforward
+    '''
+    def __init__(self, weights, bias, activation):
+        #Assign parameter inputs to class attributes
+        self.__dict__.update({k: v for k,v in locals().items() if k != self})
+
+    def feedforward(self, inputs):
+        '''
+        Returns output from given set of inputs
+
+        y = A(f(X)) where: A is an activation function and
+                            F(X) = x1*w1 + x2*w2... +xn*wn + b
+                            for weights wn and bias b
+        '''
+        total = self.linearsum(inputs)
+        if self.activation == "sigmoid":
+            return sigmoid(total)
+        elif self.activation == "ReLU":
+            return relu(total)
+        elif self.activation == "leaky ReLU":
+            return leakyrelu(total)
+        else:
+            raise Exception("Activation function not recognized")
+  
+    def linearsum(self, inputs):
+        '''
+        Returns the output before being passed throught the activation
+        function
+
+        y = f(X) where:    f(X) = x1*w1 + x2*w2... +xn*wn + b
+                            for weights wn and bias b
+        '''
+        if len(inputs) != len(self.weights):
+            raise Exception("Number of inputs must equal number of weights")
+        products = [i * w for i, w in zip(inputs, self.weights)]
+        return (sum(products) + self.bias)
+
+    def changeweight(self, newweight, index):
+        '''
+        Replaces old weight with new weight
+        '''
+        self.weights[index] = newweight
+
+    def changebias(self, newbias):
+        '''
+        Replaces old bias with new bias
+        '''
+        self.bias = newbias
+  
---
+++ weight_initialization.py
@@ -0,0 +1,46 @@
+import math
+from random import gauss
+
+def initialize(technique, fan_in, fan_out, n_weights):
+    '''
+    Calls relevant weight-initialization function
+    '''
+    if technique == "xavier":
+        return xavier(fan_in, fan_out, n_weights)
+    elif technique == "he":
+        return he(fan_in, n_weights)
+    else:
+        raise Exception("Initialization technique not recognized")
+
+
+''' Xavier-normal Initialization '''
+def xavier(n1, n2, w):
+    '''
+    Returns a gaussian-normal list with a mean of 0 and
+    a Variance of sqrt( 2 / (fan-in + fan out))
+
+    Used with Sigmoid and Softmax activations
+    '''
+    weights = []
+    variance = math.sqrt(2 / (n1 + n2))
+    sigma = math.sqrt(variance) #standard deviation
+    for i in range(w):
+        weights.append(gauss(0, sigma))
+    return weights
+
+
+''' He-normal Initialization '''
+def he(n, w):
+    '''
+    Returns a gaussian-normal list with a mean of 0 and
+    a Variance of sqrt(2 / fan-in)
+
+    Used with ReLU and leaky ReLU activations
+    '''
+    weights = []
+    variance = math.sqrt(2/n)
+    sigma = math.sqrt(variance) #standard deviation
+    for i in range(w):
+        weights.append(gauss(0, sigma))
+    return weights
+
